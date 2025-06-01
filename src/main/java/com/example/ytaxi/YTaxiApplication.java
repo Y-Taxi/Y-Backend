3127c9c2bf92c2fcd698afefd87263853f124ba6
김병훈 <gns3967@cucon.kr>
2025-06-01 16:17:34 +0900

    [MAIN] create project

---
+++ src/main/java/com/example/ytaxi/YTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ytaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YTaxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YTaxiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ytaxi/YTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ytaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YTaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
