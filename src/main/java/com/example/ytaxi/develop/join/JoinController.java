2049e0549f532605a3ba92a36c00c248bd3ca086
experthoon <kimkt1377@naver.com>
2025-08-08 17:46:36 +0900

    깃 브렌치 테스트

--- src/main/java/com/example/ytaxi/develop/join/JoinController.java
+++ src/main/java/com/example/ytaxi/develop/join/JoinController.java
@@ -21,7 +21,6 @@ public class JoinController {
 
     @RequestMapping(value = "/userJoin", method={RequestMethod.POST})
     public Map<String, Object> userJoin(@RequestBody JoinDTO joinDTO) throws Exception {
-        System.out.println("깃 브랜치 커밋 테스트 ");
         Map<String, Object> userJoinMap = new HashMap<>();
 
         try {
